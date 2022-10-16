8529ce83a1b1c987af856695695f07566b2695e0
nuria-barea <nuriab@gmail.com>
2022-10-16 18:48:25 +0200

    #7 routes ok

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import "./index.css";
 import Home from "./components/home/Home";
 import Header from "./components/header/Header";
@@ -5,21 +6,20 @@ import Movies from "./components/films/Movies";
 import TvShows from "./components/films/TvShows";
 import MoviesDetails from "./components/details/MoviesDetails";
 import TvShowsDetails from "./components/details/TvShowsDetails";
-import { BrowserRouter as Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 export default function App() {
 
   return  (
     <div className="App">
       <Header />
-      <Home />
-      {/*<Routes>
+      <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/movies" element={<Movies />} />
         <Route path="/tvshows" element={<TvShows />} />
         <Route path="/movies/:id" element={<MoviesDetails />} />
         <Route path="/tvshows/:id" element={<TvShowsDetails />} />
-      </Routes>*/}
+      </Routes>
     </div>
   );
 }
