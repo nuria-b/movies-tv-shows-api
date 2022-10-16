c6d95b6bd7913d4b02efb11e9711a75bb4fd2adf
nuria-barea <nuriab@gmail.com>
2022-10-16 18:01:04 +0200

    #7 adding routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,14 +5,20 @@ import Movies from "./components/films/Movies";
 import TvShows from "./components/films/TvShows";
 import MoviesDetails from "./components/details/MoviesDetails";
 import TvShowsDetails from "./components/details/TvShowsDetails";
+import { BrowserRouter as Routes, Route } from "react-router-dom";
 
-function App() {
+export default function App() {
 
   return  (
     <div className="App">
       <Header />
+      {/*<Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/tvshows" element={<TvShows />} />
+        <Route path="/movies/:id" element={<MoviesDetails />} />
+        <Route path="/tvshows/:id" element={<TvShowsDetails />} />
+      </Routes>*/}
     </div>
   );
 }
-
-export default App;
