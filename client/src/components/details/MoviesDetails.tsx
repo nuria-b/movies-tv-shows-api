228475459d4de67df0c0ddae25ed4f9d6071f7e3
nuria-barea <nuriab@gmail.com>
2022-10-17 12:42:06 +0200

    #3 preparing movie's details functionality

--- client/src/components/details/MoviesDetails.tsx
+++ client/src/components/details/MoviesDetails.tsx
@@ -1,6 +1,24 @@
 import "./style.css";
+import React, {useState, useEffect} from 'react';
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
 function MoviesDetails() {
+  const {id} = useParams()
+  const [oneMovie, setOneMovie] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    setLoading(true)
+    axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${
+      import.meta.env.VITE_API_KEY
+    }&language=en-US&page=1`)
+      .then(res => {
+        console.log(res.data)
+        setOneMovie(res.data);
+        setLoading(false)
+      })
+  }, [])
 
   return  (
     <div className="MoviesDetails">
