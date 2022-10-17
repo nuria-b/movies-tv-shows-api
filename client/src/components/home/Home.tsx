13eca01f3739844aaee4c04ce532ff00c1e77494
nuria-barea <nuriab@gmail.com>
2022-10-17 10:58:57 +0200

    #1 #2 preparing .env with api key

--- client/src/components/home/Home.tsx
+++ client/src/components/home/Home.tsx
@@ -1,11 +1,12 @@
 import "./style.css";
+import dataMovies from "../movies.json";
+import dataTvShows from "../tvshows.json";
+
 
 function Home() {
-
-  return  (
+  return (
     <div className="home">
       <h1 className="home-text">I'm the Home</h1>
-
     </div>
   );
 }
