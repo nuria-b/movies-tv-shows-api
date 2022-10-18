9a7364eaa804141f4186cb479efbfee31dd03ff6
nuria-barea <nuriab@gmail.com>
2022-10-18 10:49:03 +0200

    #3 #8 solving error in movie's detail axios

--- client/src/components/details/MoviesDetails.tsx
+++ client/src/components/details/MoviesDetails.tsx
@@ -8,7 +8,19 @@ function MoviesDetails() {
   const [oneMovie, setOneMovie] = useState([])
   const [loading, setLoading] = useState(false)
 
-  useEffect(() => {
+  const GetMovieById = {   
+    async getOneMovie(id:any){ 
+        setLoading(true)
+       const result = await axios.get (`https://api.themoviedb.org/3/movie/{movie_id}?api_key=${import.meta.env.VITE_API_KEY}&language=en-US/movies/${id}`)
+       .then(res => {
+        console.log(res.data)
+        setOneMovie(res.data);
+        setLoading(false)
+      }) 
+    }
+  }
+
+  /*useEffect(() => {
     setLoading(true)
     axios.get(`https://api.themoviedb.org/3/movie/{movie_id}?api_key=${import.meta.env.VITE_API_KEY}&language=en-US`)
       .then(res => {
@@ -16,15 +28,22 @@ function MoviesDetails() {
         setOneMovie(res.data);
         setLoading(false)
       })
-  }, [])
+  }, [])*/
 
-  const findMovies = oneMovie.filter(movies => movies.title === id);
+  useEffect(() => {
+    GetMovieById.getOneMovie(id).then((res:any) => {
+      let movie = res;
+      setOneMovie(movie);
+    });
+  }, []);
+
+  //const findMovies = oneMovie.filter(movies => movies.title === id);
 
   if (loading) return <section>Cargando...</section>
 
   return  (
     <section className="MoviesDetails">
-        <h1>I'm the MoviesDetails</h1>
+        <h1>I'm the movie detail{/*{oneMovie.title}*/}</h1>
     </section>
   );
 }
