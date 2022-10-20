94a447d3967742616ef58cd638e13dd72019b9d5
nuria-barea <nuriab@gmail.com>
2022-10-20 19:19:25 +0200

    #3 basic info of movie's detail done

--- client/src/components/details/MoviesDetails.tsx
+++ client/src/components/details/MoviesDetails.tsx
@@ -2,6 +2,12 @@ import "./style.css";
 import React, {useState, useEffect} from 'react';
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import Typography from "@mui/material/Typography";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Card from "@mui/material/Card";
+import { Container, Box } from "@mui/material";
+
 function MoviesDetails() {
   const {id}: any = useParams()
   const [oneMovie, setOneMovie] = useState([id])
@@ -17,12 +23,23 @@ function MoviesDetails() {
         setLoading(false)
       })
   }, [])
-  //const findMovies = oneMovie.filter(movies => movies.title === id);
+
   if (loading) return <section>Cargando...</section>
+
   return  (
+
     <section className="MoviesDetails">
-        <h1>{oneMovie.title}</h1>
+        <h1>Title: {oneMovie.title}</h1>
+        <img src={`https://image.tmdb.org/t/p/w500${oneMovie.backdrop_path}`} alt={oneMovie.title} />
+        <p>Genres:</p>
+        <p>Release date: {oneMovie.release_date}</p>
+        <p>Duration (mins): {oneMovie.runtime}</p>
+        <p>Overview: {oneMovie.overview}</p>
+        <p>Votes: {oneMovie.vote_average}</p>
+        <p>Favourites: {oneMovie.vote_count}</p>
+        <section>Related movies:</section>
     </section>
+    
   );
 }
 export default MoviesDetails;
--- client/src/components/films/Movies.tsx
+++ client/src/components/films/Movies.tsx
@@ -1,12 +1,13 @@
 import "./style.css";
 import { useEffect, createContext, useState } from "react";
 import { Link } from "react-router-dom";
+
 // Crear context de Movies
 export const MoviesContext = createContext(Movies);
 export default function Movies() {
   const [moviesList, setMoviesList] = useState([]);
   // Recoger las movies de la api
-  const fetchMoviesList = async (res:any) => {
+  const fetchMoviesList = async (res: any) => {
     const response = await fetch(
       `https://api.themoviedb.org/3/movie/popular?api_key=${
         import.meta.env.VITE_API_KEY
@@ -14,29 +15,29 @@ export default function Movies() {
     );
     const movies = await response.json();
     setMoviesList(movies.results); //mostras las movies
-    };
-    useEffect(() => {
-      fetchMoviesList(Movies);
-    }, []);
-    const value:any = {
-      moviesList,
-      setMoviesList,
-    };
+  };
+  useEffect(() => {
+    fetchMoviesList(Movies);
+  }, []);
+  const value: any = {
+    moviesList,
+    setMoviesList,
+  };
   return (
     <MoviesContext.Provider value={value}>
-      <section className="films-container" >
-        {moviesList.map((movie:any, i:any) => (
-        <section className="films-card" key={i}>
-          <Link to={`/movies/${i}`}>
-            <img
-              src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
-              alt={movie.title}
-            />
-            <h2>{movie.title}</h2>
-            <h3>Ranking: {movie.vote_average}</h3>
-          </Link>
-        </section>
-      ))}
+      <section className="films-container">
+        {moviesList.map((movie: any, i: any) => (
+          <section className="films-card" key={i}>
+            <Link to={`/movies/${i}`}>
+              <img
+                src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
+                alt={movie.title}
+              />
+              <h2>{movie.title}</h2>
+              <h3>Ranking: {movie.vote_average}</h3>
+            </Link>
+          </section>
+        ))}
       </section>
     </MoviesContext.Provider>
   );
