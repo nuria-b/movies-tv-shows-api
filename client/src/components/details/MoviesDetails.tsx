35adb9d615be4082e04e6d8d21b7a9ed0f0429ac
nuria-barea <nuriab@gmail.com>
2022-10-22 23:15:42 +0200

    #5 related movies in process

--- client/src/components/details/MoviesDetails.tsx
+++ client/src/components/details/MoviesDetails.tsx
@@ -30,23 +30,6 @@ function MoviesDetails() {
     );
   }, []);
 
-  // https://api.themoviedb.org/3/movie/{movie_id}/similar?api_key=${import.meta.env.VITE_API_KEY}&language=en-US&page=1
-
-  // Recoger las movies relacionadas de la api v1
-  useEffect(() => {
-    setLoading(true);
-    axios
-      .get(
-        `https://api.themoviedb.org/3/movie/616820/similar?api_key=${import.meta.env.VITE_API_KEY}&language=en-US&page=1`
-      )
-      .then((res: any) => {
-        //console.log(res.data.results[id])
-        setRelatedMovies(res.data.results[id]);
-        setLoading(false);
-      }
-    );
-  }, []);
-
   // Recoger los genres de la api v2
   /*
   const { genres=[] }:any = oneMovie;
@@ -78,6 +61,24 @@ function MoviesDetails() {
     setGenresList,
   };
 
+  // https://api.themoviedb.org/3/movie/{movie_id}/similar?api_key=${import.meta.env.VITE_API_KEY}&language=en-US&page=1
+  //id de prueba: 616820
+
+  // Recoger las movies relacionadas de la api v1 ---> falta cambiar 616820 por el id de la movie
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get(
+        `https://api.themoviedb.org/3/movie/${oneMovie.id}/similar?api_key=${import.meta.env.VITE_API_KEY}&language=en-US&page=1`
+      )
+      .then((res: any) => {
+        //console.log(res.data.results[id])
+        setRelatedMovies(res.data.results[id]);
+        setLoading(false);
+      }
+    );
+  }, []);
+
   // prueba para recoger info de genres
   const dataGenres = [
     { id: 28, name: "Action" },
@@ -106,6 +107,7 @@ function MoviesDetails() {
   return (
     
     <section className="FilmsDetails">
+      <h1>Id: {oneMovie.id}</h1>
       <h1>Title: {oneMovie.title}</h1>
       <img
         src={`https://image.tmdb.org/t/p/w500${oneMovie.backdrop_path}`}
