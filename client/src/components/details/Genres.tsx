18b075c90d30b6f7377c1a60a18fc365b8369505
nuria-barea <nuriab@gmail.com>
2022-10-22 10:17:47 +0200

    #3 movies genres in process

--- client/src/components/details/Genres.tsx
+++ client/src/components/details/Genres.tsx
@@ -3,29 +3,8 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 
-function Genres() {
-  // haciendo prueba para recoger info de genres
-  const dataGenres = [
-    { id: 28, name: "Action" },
-    { id: 12, name: "Adventure" },
-    { id: 16, name: "Animation" },
-    { id: 35, name: "Comedy" },
-    { id: 80, name: "Crime" },
-    { id: 99, name: "Documentary" },
-    { id: 18, name: "Drama" },
-    { id: 10751, name: "Family" },
-    { id: 14, name: "Fantasy" },
-    { id: 36, name: "History" },
-    { id: 27, name: "Horror" },
-    { id: 10402, name: "Music" },
-    { id: 9648, name: "Mystery" },
-    { id: 10749, name: "Romance" },
-    { id: 878, name: "Science Fiction" },
-    { id: 10770, name: "TV Movie" },
-    { id: 53, name: "Thriller" },
-    { id: 10752, name: "War" },
-    { id: 37, name: "Western" },
-  ];
+function Genres(props) {
+
   //    const [genresList, setGenresList] = useState([]);
 
   // Recoger las movies de la api
@@ -47,22 +26,8 @@ function Genres() {
       setGenresList,
     };
     */
-
   return (
-    <div>
-      {dataGenres.map((genre:any, i:any) => {
-        <span key={i} className="genres-item">
-          {genre.name}
-        </span>;
-      })}
-      {/*{genresList.genres &&
-            genresList.genres.slice(0, 5).map((genre:any, i:any) => (
-              <span key={i} className="genres-item">
-                {genre.name}
-              </span>
-            ))
-          }*/}
-    </div>
+    <p>{props.genre.name}</p>
   );
 }
 export default Genres;
--- client/src/components/details/MoviesDetails.tsx
+++ client/src/components/details/MoviesDetails.tsx
@@ -46,6 +46,29 @@ function MoviesDetails() {
   };
   */
 
+  // haciendo prueba para recoger info de genres
+  const dataGenres = [
+    { id: 28, name: "Action" },
+    { id: 12, name: "Adventure" },
+    { id: 16, name: "Animation" },
+    { id: 35, name: "Comedy" },
+    { id: 80, name: "Crime" },
+    { id: 99, name: "Documentary" },
+    { id: 18, name: "Drama" },
+    { id: 10751, name: "Family" },
+    { id: 14, name: "Fantasy" },
+    { id: 36, name: "History" },
+    { id: 27, name: "Horror" },
+    { id: 10402, name: "Music" },
+    { id: 9648, name: "Mystery" },
+    { id: 10749, name: "Romance" },
+    { id: 878, name: "Science Fiction" },
+    { id: 10770, name: "TV Movie" },
+    { id: 53, name: "Thriller" },
+    { id: 10752, name: "War" },
+    { id: 37, name: "Western" },
+  ];
+
   if (loading) return <section>Cargando...</section>;
 
   return (
@@ -55,7 +78,10 @@ function MoviesDetails() {
         src={`https://image.tmdb.org/t/p/w500${oneMovie.backdrop_path}`}
         alt={oneMovie.title}
       />
-      <p>Genres: {<Genres/>}</p>
+      <div>{dataGenres.map((g)=>{
+	        return <Genres genre={g}/>
+        })}
+      </div>
       <p>Release date: {oneMovie.release_date}</p>
       <p>Duration (mins): {oneMovie.runtime}</p>
       <p>Overview: {oneMovie.overview}</p>
