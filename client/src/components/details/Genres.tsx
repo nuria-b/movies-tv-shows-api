92d5480210e59bff4ad87f05a2e486bf6e32c49b
nuria-barea <nuriab@gmail.com>
2022-10-22 09:29:08 +0200

    #3 movies genres in process

--- client/src/components/details/Genres.tsx
+++ client/src/components/details/Genres.tsx
@@ -4,10 +4,33 @@ import { useParams } from "react-router-dom";
 import axios from "axios";
 
 function Genres() {
-    const [genresList, setGenresList] = useState([]);
+
+    // haciendo prueba para recoger info de genres
+    const dataGenres = [
+        { "id": 28, "name": "Action" },
+      { "id": 12, "name": "Adventure" },
+      { "id": 16, "name": "Animation" },
+      { "id": 35, "name": "Comedy" },
+      { "id": 80, "name": "Crime" },
+      { "id": 99, "name": "Documentary" },
+      { "id": 18, "name": "Drama" },
+      { "id": 10751, "name": "Family" },
+      { "id": 14, "name": "Fantasy" },
+      { "id": 36, "name": "History" },
+      { "id": 27, "name": "Horror" },
+      { "id": 10402, "name": "Music" },
+      { "id": 9648, "name": "Mystery" },
+      { "id": 10749, "name": "Romance" },
+      { "id": 878, "name": "Science Fiction" },
+      { "id": 10770, "name": "TV Movie" },
+      { "id": 53, "name": "Thriller" },
+      { "id": 10752, "name": "War" },
+      { "id": 37, "name": "Western" }
+    ]
+//    const [genresList, setGenresList] = useState([]);
   
     // Recoger las movies de la api
-    
+/*    
     const fetchGenresList = async (res: any) => {
       const response = await fetch(
         `https://api.themoviedb.org/3/genre/movie/list?api_key=${import.meta.env.VITE_API_KEY}&language=en-US`
@@ -24,16 +47,16 @@ function Genres() {
       genresList,
       setGenresList,
     };
-    
+    */
     return (
         <p>Genres:
-          {genresList.genres &&
+          {/*{genresList.genres &&
             genresList.genres.slice(0, 5).map((genre:any, i:any) => (
               <span key={i} className="genres-item">
                 {genre.name}
               </span>
             ))
-          }
+          }*/}
         </p>
     );
   }
