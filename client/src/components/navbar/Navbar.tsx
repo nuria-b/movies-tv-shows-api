a72cfa258060e934580f9475a47def4b503679a6
nuria-barea <nuriab@gmail.com>
2022-10-27 19:10:26 +0200

    #2 #3 #4 #5 appling buttons change color when active

--- client/src/components/navbar/Navbar.tsx
+++ client/src/components/navbar/Navbar.tsx
@@ -3,37 +3,37 @@ import React, { useState } from "react";
 import "./style.css";
 
 function Navbar() {
-  // grab all the buttons
+  // Recoger todos los botones
   let Buttons = document.querySelectorAll(".selectSection button");
 
-  // loop through the buttons using for..of
+  // Recorrer los botones
   for (let button of Buttons) {
-    // listen for a click event
+    // Activar la escucha para un evento de click
     button.addEventListener("click", (e) => {
-      // et = event target
+      // Usar et como diminutivo de event target
       const et: any = e.target;
-      // slect active class
+      // Seleccionar la clase activa
       const active: any = document.querySelector(".active");
-      // check for the button that has active class and remove it
+      // Verificar el botón que tiene una clase activa   
       if (active) {
         active.classList.remove("active");
       }
-      // add active class to the clicked element
+      // Añadir la clase activa al botón clicado
       et.classList.add("active");
 
-      // select all classes with the name content
+      // Seleccionar todas las clases llamadas content
       let allContent: any = document.querySelectorAll(".content");
 
-      // loop through all content classes
+      // Recorrer todas las clases de contenido
       for (let content of allContent) {
-        // display the content if the class has the same data-attribute as the button
+        // Mostrar el contenido si la clase tiene el mismo atributo de datos que el botón
         if (
           content.getAttribute("data-number") ===
           button.getAttribute("data-number")
         ) {
           content.style.display = "block";
         }
-        // if it's not equal then hide it.
+        // Esconder el contenido si la clase no tiene el mismo atributo de datos que el botón
         else {
           content.style.display = "none";
         }
