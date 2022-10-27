3570b6fd5168d71a4a4b8b388127af405ed65465
nuria-barea <nuriab@gmail.com>
2022-10-27 13:09:40 +0200

    #2 #3 #4 #5 appling buttons change color when active

--- client/src/components/navbar/Navbar.tsx
+++ client/src/components/navbar/Navbar.tsx
@@ -1,22 +1,34 @@
 import { Link } from "react-router-dom";
-import Movies from "../films/Movies";
-import TvShows from "../films/TvShows";
+import React, { useState } from "react";
 import "./style.css";
 
 function Navbar() {
+  // mantener el color negro del botón activo al hacer click
+  // const [activeMovie, setActiveMovie] = useState(false);
+  // const [activeTvShow, setActiveTvShow] = useState(false);
+
+  // const handleClickMovies = () => {
+  //   setActiveMovie(!activeMovie);
+  // };
+  // const handleClickTvShows = () => {
+  //   setActiveTvShow(!activeTvShow);
+  // };
+
   return (
     <nav>
       <ul>
         <li>
-          <Link className="Nav-active" to="/movies">
-            <button>
+          <Link className="nav-active" to="/movies">
+            <button {/*onClick={handleClickMovies}
+        style={{ backgroundColor: activeMovie ? "black" : "white" }}*/}>
               <p className="menu-text">Movies</p>
             </button>
           </Link>
         </li>
         <li>
-          <Link className="Nav-active" to="/tvshows">
-            <button>
+          <Link className="nav-active" to="/tvshows">
+            <button {/*onClick={handleClickTvShows}
+        style={{ backgroundColor: activeTvShow ? "black" : "white" }}*/}>
               <p className="menu-text">TV Shows</p>
             </button>
           </Link>
