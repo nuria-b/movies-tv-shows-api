c92edab7c6f0a04ef8291e126c961b66bb6bc2d0
nuria-barea <nuriab@gmail.com>
2022-10-28 09:28:59 +0200

    #2 #3 #4 #5 #8 solving errors in buttons change color when active

--- client/src/components/navbar/Navbar.tsx
+++ client/src/components/navbar/Navbar.tsx
@@ -2,44 +2,43 @@ import { Link } from "react-router-dom";
 import React, { useState } from "react";
 import "./style.css";
 
-function Navbar() {
+export default function Navbar() {
   // Recoger todos los botones
   let Buttons = document.querySelectorAll(".selectSection button");
 
-  // Recorrer los botones
-  for (let button of Buttons) {
-    // Activar la escucha para un evento de click
-    button.addEventListener("click", (e) => {
-      // Usar et como diminutivo de event target
-      const et: any = e.target;
-      // Seleccionar la clase activa
-      const active: any = document.querySelector(".active");
-      // Verificar el botón que tiene una clase activa   
-      if (active) {
-        active.classList.remove("active");
-      }
-      // Añadir la clase activa al botón clicado
-      et.classList.add("active");
+  // for (let button of Buttons) {
+  //   // Activar la escucha para un evento de click
+  //   button.addEventListener("click", (e) => {
+  //     // Usar et como diminutivo de event target
+  //     const et: any = e.target;
+  //     // Seleccionar la clase activa
+  //     const active: any = document.querySelector(".active");
+  //     // Verificar el botón que tiene una clase activa   
+  //     if (active) {
+  //       active.classList.remove("active");
+  //     }
+  //     // Añadir la clase activa al botón clicado
+  //     et.classList.add("active");
 
-      // Seleccionar todas las clases llamadas content
-      let allContent: any = document.querySelectorAll(".content");
+  //     // Seleccionar todas las clases llamadas content
+  //     let allContent: any = document.querySelectorAll(".content");
 
-      // Recorrer todas las clases de contenido
-      for (let content of allContent) {
-        // Mostrar el contenido si la clase tiene el mismo atributo de datos que el botón
-        if (
-          content.getAttribute("data-number") ===
-          button.getAttribute("data-number")
-        ) {
-          content.style.display = "block";
-        }
-        // Esconder el contenido si la clase no tiene el mismo atributo de datos que el botón
-        else {
-          content.style.display = "none";
-        }
-      }
-    });
-  }
+  //     // Recorrer todas las clases de contenido
+  //     for (let content of allContent) {
+  //       // Mostrar el contenido si la clase tiene el mismo atributo de datos que el botón
+  //       if (
+  //         content.getAttribute("data-number") ===
+  //         button.getAttribute("data-number")
+  //       ) {
+  //         content.style.display = "block";
+  //       }
+  //       // Esconder el contenido si la clase no tiene el mismo atributo de datos que el botón
+  //       else {
+  //         content.style.display = "none";
+  //       }
+  //     }
+  //   });
+  // }
 
   return (
     <nav>
@@ -62,5 +61,3 @@ function Navbar() {
     </nav>
   );
 }
-
-export default Navbar;
